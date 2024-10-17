ed952e918c1e1b4869dec5b970d23e39d1964730
Naveenbalajis81 <naveenbalajis.aids22@veltechmultitech.org>
2024-10-17 22:03:25 +0530

    python

--- python.py
+++ python.py
@@ -19,17 +19,17 @@ while(y<=c and x<=r):
         t+=1
     x+=1
 
-    for i in range(y,c,+1):
+    for i in range(x,r,+1):
         l[i][r-1]=t
         t+=1
     c-=1
 
-    for i in range(y,c,+1):
+    for i in range(c,y,-1):
         l[c][i-1]=t
         t+=1
     r-=1
 
-    for i in range(y,c,+1):
+    for i in range(r,x,-1):
         l[i-1][y]=t
         t+=1
     y+=1
@@ -38,10 +38,10 @@ for i in range(n):
     for j in range(n):
         print(l[i][j],end=" ")
         if(l[i][j]%11==0):
-            pp.append(i,j)
-        print()
+            pp.append((i,j))
+    print()
 
-print("total power points:",+str(len(pp)+1))
+print("total power points:"+str(len(pp)+1))
 print("(0,0)")
 for i in pp:
     print(i)
